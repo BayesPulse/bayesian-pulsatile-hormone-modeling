8f9273918b3672276c7863f9088c731641bfc4af
Matt Mulvahill (peregrine) <mjmulvahill@gmail.com>
2017-06-26 15:27:20 -0600

    added code for automatically building via travis-ci

---
+++ _deploy.sh
@@ -0,0 +1,20 @@
+# Set git config information
+git config --global user.name "Matt Mulvahill"
+git config --global user.email "matthew.mulvahill@ucdenver.edu"
+
+# Clone the gh-pages repository
+git clone -b gh-pages \
+    https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+      book-output
+
+# Change to the gh-page clone book-output directory
+cd book-output
+
+# Copy generated output to book-output
+cp -r ../_book/* ./
+
+# Add all files to the repo
+git add *
+git commit -a -m "Updating book (${TRAVIS_BUILD_NUMBER})"
+git push -q origin gh-pages
+
