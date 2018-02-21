f99abee330ce91e2894f6485d4f4d044b8583b5c
Matt <mjmulvahill@gmail.com>
2018-02-21 15:30:00 -0700

    i think this will do it -- needed to assign pat to a bash variable name when encrypting.

--- _deploy.sh
+++ _deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 set -e
-set -x
 
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
@@ -11,12 +10,8 @@ git config --global user.name "Matt Mulvahill"
 
 git clone -b master https://${GITHUB_PAT}@github.com/BayesPulse/bayespulse.github.io.git book-output
 cd book-output
-pwd
-ls -la
 cp -r ../_book/* ./
 git add --all *
-git status
-ls -la
 git commit -m "Update the book" || true
 git push -q origin master
 
