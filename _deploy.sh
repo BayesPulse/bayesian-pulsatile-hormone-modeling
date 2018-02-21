02a16ffcb8b9fa2cf5f73f8b31c600b1c3e6823c
Matt <mjmulvahill@gmail.com>
2018-02-21 15:15:22 -0700

    mor debugging

--- _deploy.sh
+++ _deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
+set -x
 
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
@@ -10,7 +11,12 @@ git config --global user.name "Matt Mulvahill"
 
 git clone -b master https://${GITHUB_PAT}@github.com/BayesPulse/bayespulse.github.io.git book-output
 cd book-output
+pwd
+ls -la
 cp -r ../_book/* ./
 git add --all *
+git status
+ls -la
 git commit -m "Update the book" || true
 git push -q origin master
+
